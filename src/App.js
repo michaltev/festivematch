76d0284eded5a846248c3838f213680a612f55c6
michaltev <michaltev@gmail.com>
2020-03-08 16:18:35 +0200

    particles cool graphycs added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React from 'react';
+import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import FavoriteBand from './components/FavoriteBand/FavoriteBand';
 import ChooseArtistsForm from './components/ChooseArtistsForm/ChooseArtistsForm';
 import './App.css';
 
+const particlesParams = {
+  particles: {
+    number:{
+      value:200,
+      density:{
+        enable:true,
+        value_area:800
+      }
+    }
+  }
+};
+
 function App() {
   return (
     <div className="App">
-      
+        <Particles className='particles' params={particlesParams} />
         <Navigation />
         <Logo />
         <FavoriteBand/>
