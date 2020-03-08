b7e5e6cd610589adab7dbb7df58f22b644765338
michaltev <michaltev@gmail.com>
2020-03-08 15:08:14 +0200

    logo component added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import Navigation from './components/Navigation/Navigation'
+import Logo from './components/Logo/Logo'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      {
+      
         <Navigation />
-        /*<Logo />
-        <ChooseArtistsForm />
+        <Logo />
+        {
+        /*<ChooseArtistsForm />
         <FestivalList />
         <FestivalTable />
 
