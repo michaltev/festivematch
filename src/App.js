05e432a89a22fe52201fd1638d070ad90cb636f5
michaltev <michaltev@gmail.com>
2020-03-08 14:04:36 +0200

    adding components  foldres and basic js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     
+      {/*
+        <Navigation />
+        <Logo />
+        <ChooseArtistsForm />
+        <FestivalList />
+        <FestivalTable />
+
+      */}
     </div>
   );
 }
