20e5ad13a7564f7c03fc1e464de48191fce4548d
michaltev <michaltev@gmail.com>
2020-03-09 14:02:33 +0200

    empty tag in the result components

--- src/components/FestivalList/FestivalList.js
+++ src/components/FestivalList/FestivalList.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const FestivalList = () =>
-{};
+{
+	return (<p/>);
+};
 
 
 export default FestivalList;
--- src/components/FestivalTable/FestivalTable.js
+++ src/components/FestivalTable/FestivalTable.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
 const FestivalTable = () =>
-{};
+{
+	return (<p/>);
+};
 
 export default FestivalTable;
