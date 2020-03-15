73911a38f6c187a59a7d7326adb9c26c9dfed356
michaltev <michaltev@gmail.com>
2020-03-15 21:21:11 +0200

    basic server side fetching

--- src/App.js
+++ src/App.js
@@ -61,6 +61,12 @@ class App extends Component {
     }
   }
 
+  componentDidMount(){
+    fetch('http://localhost:3000/')
+    .then(response => response.json())
+    .then(console.log);
+  }
+
   onRouteChanged = (route) => {
     if(route === 'signout')
     {
