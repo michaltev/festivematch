f2df49ebe1a551cc66183d8d1fb9cd9b60cdb817
michaltev <michaltev@gmail.com>
2020-03-26 16:39:13 +0200

    Update ChooseArtistsForm.js
    some text added for UX

--- src/components/ChooseArtistsForm/ChooseArtistsForm.js
+++ src/components/ChooseArtistsForm/ChooseArtistsForm.js
@@ -5,15 +5,20 @@ const ChooseArtistsForm = ({onInputChange, onButtonSubmit}) =>
 {
 	return (
 		<div>
-			<p className='f3'>
-			{'This is gonna find your fastival'}
-			</p>
 			<div className='center'>
 				<div className='form center pa4 br3 shadow-5'>
+					<p className='f3 white ma1'>
+						{`Any other artists you're interested in?`}
+					</p>
 					<input className='f4 pa2 w-70 center' type='text' onChange={onInputChange} />
-					<button className='f4 grow w-30 link ph3 pv2 dib white bg-pink' onClick={onButtonSubmit}>Find by artists</button>
+					<button className='f4 grow w-30 link ph3 pv2 dib white bg-pink' onClick={onButtonSubmit}>
+						Find by artists
+					</button>
 				</div>
 			</div>
+			<p className='f3'>
+			{'Lets find you a fastival!'}
+			</p>
 		</div>
 
 		)
