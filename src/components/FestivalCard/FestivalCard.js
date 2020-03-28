cf915aac9cc08b46d9a9bab86304e9eecb8b4d4e
michaltev <michaltev@gmail.com>
2020-03-29 00:01:52 +0300

    cancled festivals are written in red

--- src/components/FestivalCard/FestivalCard.js
+++ src/components/FestivalCard/FestivalCard.js
@@ -6,7 +6,7 @@ const FestivalCard = ({ festival }) =>
     <div className='tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5 w-30'>
       <img alt='festival' src={require(`../../Images/${Math.floor(Math.random() * 4) + 1}.jpg`)}/> 
       <div>
-        <h2>{festival.displayName}</h2>
+        <h2 className={festival.status !== "ok" ? "red" : "black"}>{festival.displayName}</h2>
         <h3>{festival.location.city}</h3>
         <h4>Start: {festival.start.date} | End: {festival.end.date}</h4>
         <p> {Math.round((festival.popularity + Number.EPSILON) * 100) / 10}/10 popularity </p>
